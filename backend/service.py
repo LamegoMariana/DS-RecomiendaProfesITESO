9557018eef3bde7a0d9e3147e1fe58f537117921
alberto romano <aromanog1@gmail.com>
2023-11-15 18:08:28 -0600

    fresh start

--- backend/service.py
+++
@@ -1,7 +0,0 @@
-from it_service import ServiceInterface
-
-class Service(ServiceInterface):
-	def __init__(self):
-		pass
-	def sign_in(self, email:str, password:str):
-		return 'Algo?'
--- backend/student.py
+++
@@ -1,6 +0,0 @@
-from it_user_type import UserTypeInterface
-
-class Student(UserTypeInterface):
-	''' Concrete class that uses UserTypeInterface '''
-	def __init__(self, name):
-		super().__init__(name)
