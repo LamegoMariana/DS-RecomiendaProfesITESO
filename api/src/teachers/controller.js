5cc4816da792076ece33066d2fa84d9820f2a255
alberto romano <aromanog1@gmail.com>
2023-11-15 23:30:18 -0600

    forgot to include this in last commit

--- api/src/teachers/controller.js
+++ api/src/teachers/controller.js
@@ -1,15 +1,48 @@
 const teacherSchema = require('./schema');
+const mongo = require('mongodb');
 
 class TeachersController {
+	static transformData(data) {
+		const transformedData = data.map((item) => ({
+			...item.toObject(),
+			id: item._id.toString(), // Convert ObjectId to string
+		}));
+
+		return transformedData;
+	}
+
 	async index(req, res) {
 		try {
 			let teachers = await teacherSchema.find();
 
-			if (!teachers) throw Error('No teachers found');
+			const transformedData = TeachersController.transformData(teachers);
 
-			res.status(200).json(teachers);
+			res.status(200).json(transformedData);
 		} catch (err) {
 			res.status(500).json({ msg: err });
+		}
+	}
+
+	async show(req, res) {
+		try {
+			const mongoID = new mongo.ObjectId(req.params.id);
+
+			// Use findOne with a query object containing _id
+			const teacher = await teacherSchema.findOne({ _id: mongoID });
+
+			if (!teacher) {
+				// If no teacher is found, you may want to send a 404 status code
+				return res.status(404).json({ msg: 'Teacher not found' });
+			}
+
+			// Assign the id field with the string version of _id
+			teacher.id = teacher._id.toString();
+
+			res.status(200).json(teacher);
+		} catch (err) {
+			// Log the error for debugging purposes
+			console.error(err);
+			res.status(500).json({ msg: 'Internal Server Error' });
 		}
 	}
 
--- api/src/teachers/routes.js
+++ api/src/teachers/routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const teachersController = require('./controller');
 
 router.get('', teachersController.index);
+router.get('/:id', teachersController.show);
 router.post('', teachersController.create);
 module.exports = router;
