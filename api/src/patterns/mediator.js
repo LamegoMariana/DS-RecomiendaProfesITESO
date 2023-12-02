321b6370da10e469a6e99d7ff969db05ff300385
alberto romano <aromanog1@gmail.com>
2023-12-01 21:27:56 -0600

    Added code to mediator

--- api/src/patterns/mediator.js
+++ api/src/patterns/mediator.js
@@ -1,1 +1,23 @@
-class Mediator {}
+class Mediator {
+	constructor() {
+		this.stars = 0;
+	}
+
+	grade(student, teacher, subject) {}
+
+	sortTeachers() {}
+
+	search(teacher, subject) {}
+
+	getInteractions() {
+		const interaction = {
+			comment: this.comment,
+		};
+
+		if (this.children.length > 0) {
+			interaction['responses'] = this.children.map((comment) => comment.getInteractions());
+		}
+
+		return interaction;
+	}
+}
